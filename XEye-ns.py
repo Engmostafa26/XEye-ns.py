16b067daf27d2d14ea2df3bfac0d133dd0194831
Engmostafa26 <53053339+Engmostafa26@users.noreply.github.com>
2022-03-04 22:45:29 +0200

    Update XEye-ns.py

--- XEye-ns.py
+++ XEye-ns.py
@@ -1,46 +1,40 @@
 #! /usr/bin/env python3
 
-import argparse
+import subprocess
+import re
 import scapy.all as sc
 
-print("---------------------------------------------------------")
-print(" \n[Welcoming] --> Welcome to XEye Network Scanner \n")
-print(" [Info] --> This is a simple and fast network scanner to get the hosts that are up and their Mac addresses\n\n")
-
-def args():
-    opts = argparse.ArgumentParser()
-    opts.add_argument("-t","--target",dest="the_target",help="The target IP or Ip range")
-    ip = opts.parse_args()
-    if ip.the_target:
-        scanning(ip.the_target)
+print("\n\n\n\t\t\t\t\t\t[*] Welcome to XEye Network Scanner [*]")
+print("\n\n\n[Info] --> With \"XEye-ns\" you can scan the network and find all the devices IPs and Mac addresses")
+ipr = subprocess.getoutput("ip r | grep proto | cut -d\" \" -f1")
+ipra = re.search(r"(?:\d{1,3}\.){3}\d{1,3}(?:/\d\d?)?",str(ipr)).group(0)
+def scan(ip):
+    print("\n-----------------------------------------------------------------------------------")
+    print("\n[Working] --> XEye-ns is scanning your network subnet which is \""+ipra+"\"")
+    ipa = sc.ARP(pdst=ip)
+    ipe = sc.Ether(dst="ff:ff:ff:ff:ff:ff")
+    # sc.ls(sc.Ether())
+    ipc = ipe/ipa
+    # ipc.show()
+    # print(ipc.summary())
+    an = sc.srp(ipc,timeout=3,verbose=False)[0]
+    # print(an.summary())
+    for each in an:
+        # print(each[1].show())
+        print("\n\t\t\t\t\t\t[*] --> The IP: "+each[1].psrc +"\n\t\t\t\t\t\t[*] --> The Mac: "+each[1].hwsrc+"\n")
+        print("\t\t\t\t\t\t--------------------------------------")
+def checkpriv():
+    who=subprocess.getoutput("whoami")
+    checwho = re.search(r"root",str(who))
+    if checwho is None:
+        print(" [Instruction] --> Please run the tool with \"sudo\" - Exiting ......")
     else:
-        print(" [Info] --> Entering the interactive mode ")
-        interct()
-def interct():
-    ip = input(" [Required] --> Enter the network range: ")
-    if ip:
-        scanning(ip)
-    else:
-        print(" [Warning] --> No Entry ")
-        exit()
-def scanning(ip):
-    target = sc.ARP(pdst=ip)
-    destmac = sc.Ether(dst="ff:ff:ff:ff:ff:ff")
-    comb = destmac/target
-    gotten = sc.srp(comb,timeout=3,verbose=False)[0]
-    ips = []
-    for el in gotten:
-        gottenips = {"ips":el[1].psrc,"mac":el[1].hwsrc}
-        ips.append(gottenips)
-        #print("---------------------------------------------------------")
-    printr(ips)
-def printr(ipss):
-    print("\n The IP \t\t The Mac Address\n ---------------------------------------------------------\n")
-    for nn in ipss:
-        print(nn["ips"]+"\t\t"+nn["mac"])
-args()
-print("\n---------------------------------------------------------")
-print("\n --> The Facebook OSINT Hacking course: https://www.udemy.com/course/facebook-osint-hacking/?referralCode=1FEF1A87D703B6DAE484")
-print("\n --> The Kali Linux Command course: https://www.udemy.com/course/linux-command-lines-from-a-hackers-perspective/?referralCode=62A07A01780C21117592")
-print("\n---------------------------------------------------------")
-print("\n\n [Author] --> ENG.Mostafa Ahmad - Cybersecurity Expert")
+        scan(ipra)
+def TheEnd():
+    print("\n\t\t\t\t[*] Thanks for using XEye-ns. Below are recommended courses for you :) [*]")
+    print("\n\n\n --> The Facebook OSINT Hacking course: https://www.udemy.com/course/facebook-osint-hacking/?referralCode=1FEF1A87D703B6DAE484")
+    print("\n --> The Kali Linux Command course: https://www.udemy.com/course/linux-command-lines-from-a-hackers-perspective/?referralCode=62A07A01780C21117592")
+    print("\n---------------------------------------------------------")
+    print("\n [Author] --> ENG.Mostafa Ahmad - Cybersecurity Expert and XEye founder")
+checkpriv()
+TheEnd()
